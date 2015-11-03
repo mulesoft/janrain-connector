721d64df7c84ef6a8567aaaace50398d98f0bae1
Sandipan Basu <sandipan@whiteskylabs.com>
2015-11-03 11:20:59 +0530

    CLDCONNECT-2433 - Migrated the connector to community and devkit 3.4.5

--- src/test/java/org/mule/modules/janrain/automation/testcases/engage/general/AnalyticsAccessTestCases.java
+++ src/test/java/org/mule/modules/janrain/automation/testcases/engage/general/AnalyticsAccessTestCases.java
@@ -1,15 +1,10 @@
 /**
- *
- * (c) 2003-2012 MuleSoft, Inc. This software is protected under international
- * copyright law. All use of this software is subject to MuleSoft's Master
- * Subscription Agreement (or other Terms of Service) separately entered
- * into between you and MuleSoft. If such an agreement is not in
- * place, you may not use the software.
+ * (c) 2003-2015 MuleSoft, Inc. The software in this package is published under
+ * the terms of the CPAL v1.0 license, a copy of which has been included with this
+ * distribution in the LICENSE.md file.
  */
 
 package org.mule.modules.janrain.automation.testcases.engage.general;
-
-import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.junit.After;
@@ -21,6 +16,8 @@ import org.mule.api.MuleEvent;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.modules.janrain.automation.JanrainTestParent;
 import org.mule.modules.janrain.automation.RegressionTests;
+
+import java.util.Map;
 
 public class AnalyticsAccessTestCases extends JanrainTestParent {
 	@Before
